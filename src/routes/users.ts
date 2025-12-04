2b1af7ebab8ce8738ce569c358577028027b3565
Krivega <mr.krivega@gmail.com>
2025-12-04 16:46:15 +0300

    feat: add user authentication routes for login and signup

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -10,13 +10,16 @@ import {
   updateUserAvatarByIdSchema,
   updateUserById,
   updateUserAvatarById,
+  loginSchema,
+  login,
 } from '@/controllers/users.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.get('/:userId', getUserByIdSchema, getUserById);
-router.post('/', createUserSchema, createUser);
+router.post('/signin', loginSchema, login);
+router.post('/signup', createUserSchema, createUser);
 router.put('/me', updateUserByIdSchema, updateUserById);
 router.patch('/me/avatar', updateUserAvatarByIdSchema, updateUserAvatarById);
 
