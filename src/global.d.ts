8b897f4691cf3b617e91be894d373ff6ed1ec5fb
Krivega <mr.krivega@gmail.com>
2025-12-05 14:04:01 +0300

    chore: update global type definitions and tsconfig to include additional type references

---
+++ src/global.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="jest" />
+
+import type { JwtPayload } from 'jsonwebtoken';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: JwtPayload & { userId: string };
+    }
+  }
+}
