a5833c7c43f9c7821978deddad8f67c6f5add031
Krivega <mr.krivega@gmail.com>
2025-12-05 14:17:24 +0300

    chore: update dependencies and add logging middleware

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import connectDb from '@/db.js';
 import log from '@/log.js';
 import auth from '@/middlewares/auth.js';
 import errorHandlers from '@/middlewares/errorHandlers.js';
+import { requestLogger, errorLogger } from '@/middlewares/logger.js';
 import authRouter from '@/routes/auth.js';
 import authorizedRouter from '@/routes/authorizedRouter.js';
 
@@ -14,10 +15,12 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
+app.use(requestLogger);
 app.use(authRouter);
 
 app.use(auth, authorizedRouter);
 
+app.use(errorLogger);
 app.use(errorHandlers);
 
 await connectDb().catch((error: unknown) => {
